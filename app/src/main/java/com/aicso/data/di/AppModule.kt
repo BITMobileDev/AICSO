03c0e8226699e019af44c0244038543974bd40a4
Tolulope Fanimokun <fanimokuntolulope@yahoo.com>
2025-12-11 20:45:22 +0100

    Updated the app module

--- app/src/main/java/com/aicso/data/di/AppModule.kt
+++ app/src/main/java/com/aicso/data/di/AppModule.kt
@@ -19,6 +19,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Dns
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -47,6 +48,7 @@ object AppModule {
         .readTimeout(30, TimeUnit.SECONDS)
         .writeTimeout(30, TimeUnit.SECONDS)
         .pingInterval(30, TimeUnit.SECONDS)
+        .dns(Dns.SYSTEM)
         .build()
 
     @Provides
@@ -114,81 +116,3 @@ object AppModule {
         return SignalRManager(gson)
     }
 }
-
-
-
-//
-//package com.aicso.di
-//
-//import com.aicso.BuildConfig
-//import com.aicso.data.api.ChatApiService
-//import com.aicso.data.signalr.SignalRManager
-//import com.google.gson.Gson
-//import com.google.gson.GsonBuilder
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import okhttp3.OkHttpClient
-//import okhttp3.logging.HttpLoggingInterceptor
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//import java.util.concurrent.TimeUnit
-//import javax.inject.Singleton
-//
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object NetworkModule {
-//
-//    @Provides
-//    @Singleton
-//    fun provideGson(): Gson {
-//        return GsonBuilder()
-//            .setLenient()
-//            .create()
-//    }
-//
-//    @Provides
-//    @Singleton
-//    fun provideOkHttpClient(): OkHttpClient {
-//        val loggingInterceptor = HttpLoggingInterceptor().apply {
-//            level = if (BuildConfig.DEBUG) {
-//                HttpLoggingInterceptor.Level.BODY
-//            } else {
-//                HttpLoggingInterceptor.Level.NONE
-//            }
-//        }
-//
-//        return OkHttpClient.Builder()
-//            .addInterceptor(loggingInterceptor)
-//            .connectTimeout(30, TimeUnit.SECONDS)
-//            .readTimeout(30, TimeUnit.SECONDS)
-//            .writeTimeout(30, TimeUnit.SECONDS)
-//            .build()
-//    }
-//
-//    @Provides
-//    @Singleton
-//    fun provideRetrofit(
-//        okHttpClient: OkHttpClient,
-//        gson: Gson
-//    ): Retrofit {
-//        return Retrofit.Builder()
-//            .baseUrl(BuildConfig.BASE_URL)
-//            .client(okHttpClient)
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .build()
-//    }
-//
-//    @Provides
-//    @Singleton
-//    fun provideChatApiService(retrofit: Retrofit): ChatApiService {
-//        return retrofit.create(ChatApiService::class.java)
-//    }
-//
-//    @Provides
-//    @Singleton
-//    fun provideSignalRManager(): SignalRManager {
-//        return SignalRManager()
-//    }
-//}
